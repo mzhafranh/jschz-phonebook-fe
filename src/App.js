f967811d8a4adce0e5de67d02c7b29a22383dacc
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2024-10-27 18:21:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-// import logo from './logo.svg';
 import PhonebookBox from './components/PhonebookBox';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import PhonebookList from './components/PhonebookList';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 function App() {
